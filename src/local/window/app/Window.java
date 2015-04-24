c8a4db380eceb9720e6e16a5d568e3e324d39bf3
Alex White <alex.whie@oval.uk.com>
2015-04-24 16:52:36 +0100

    upadtes to window setup

--- src/local/window/app/Window.java
+++ src/local/window/app/Window.java
@@ -1,5 +1,16 @@
 package local.window.app;
 
+import java.awt.Dimension;
+import java.awt.DisplayMode;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Point;
+import java.awt.Toolkit;
+
+import javax.swing.JPanel;
+
+import com.jogamp.newt.event.KeyEvent;
+import com.jogamp.newt.event.KeyListener;
 import com.jogamp.newt.event.WindowAdapter;
 import com.jogamp.newt.event.WindowEvent;
 import com.jogamp.newt.opengl.GLWindow;
@@ -12,22 +23,27 @@ import com.jogamp.opengl.GLProfile;
  * demo here:
  * https://github.com/sgothel/jogl/blob/master/src/test/com/jogamp/opengl/test/junit/jogl/demos/es2/av/MovieCube.java
  * 
+ * https://www.youtube.com/watch?v=fQO1tOYlkTo
+ * 
  */
 
 public class Window implements GLEventListener
 {
-
-	public Window()
-	{
-	}
+	
+	private static GraphicsEnvironment graphicsEnvironment;
+	private static boolean isFullScreen = false;
+	public static DisplayMode dm, dm_old;
+	private static Dimension xgraphic;
+	private static Point point = new Point( 0, 0 );
+	private static GLWindow window;
 
 	public static void main( String[] args )
 	{
 		GLProfile glp = GLProfile.getDefault();
 		GLCapabilities caps = new GLCapabilities( glp );
 		
-		GLWindow window = GLWindow.create( caps );
-		window.setSize( 300, 300 );
+		window = GLWindow.create( caps );
+		window.setSize( 1000, 600 );
 		window.setVisible( true );
 		window.setTitle( "JOGL window test" );
 		
@@ -36,7 +52,71 @@ public class Window implements GLEventListener
 				System.exit( 0 );
 			};
 		} );
+		
+		/**
+		 * Centre on screen
+		 */
+		
+		graphicsEnvironment = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		GraphicsDevice[] devices = graphicsEnvironment.getScreenDevices();
+		
+		dm_old = devices[0].getDisplayMode();
+		dm = dm_old;
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		
+		int windowX = Math.max( 0, (screenSize.width - window.getWidth() ) );
+		int windowY = Math.max( 0, (screenSize.height - window.getHeight() ) );
+		
+		window.setPosition( windowX, windowY );
+		
+		/**
+		 * add button control
+		 */
+		KeyListener fullScreenKey = new KeyListener() {
+				
+			@Override
+			public void keyReleased( KeyEvent e )
+			{
+								
+			}
+			
+			@Override
+			public void keyPressed( KeyEvent e )
+			{
+				switch (e.getKeyCode())
+				{
+				case KeyEvent.VK_F:
+					fullScreen();
+					break;
+				default:
+					break;
+				}
+				
+			}
+
+			
+		};
+		
 	}
+	
+	
+	private static void fullScreen()
+	{
+
+		if( !isFullScreen )
+		{
+			window.setAlwaysOnTop( true );
+			window.setFullscreen( true );
+			isFullScreen = true;
+		}
+		else
+		{
+			window.setAlwaysOnTop( true );
+			window.setFullscreen( false );
+		}
+	
+	}
+	
 
 	@Override
 	public void display( GLAutoDrawable arg0 )
